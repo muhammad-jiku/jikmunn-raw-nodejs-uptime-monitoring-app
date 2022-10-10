7a59f0f896257ff75cba53d43138036cd2aec505
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-10 19:20:13 +0600

    parsing request is added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // dependencies
 const http = require('http');
+const url = require('url');
 
 // app object
 const app = {};
@@ -20,6 +21,17 @@ app.createServer = () => {
 
 // handle request and response
 app.handleReqRes = (req, res) => {
+  // requesting url handling
+  // get the url and parse it
+  const parsedUrl = url.parse(req.url, true); // parse url receives two parameters. one of them is requested url and other one is thoe boolean statement. if its "true" its gonna take everything from url like qurey and if its "false" its gonna take only the url
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headersObject = req.headers;
+
+  console.log(headersObject);
+
   // response handle
   res.end('Hello World!!');
 };
