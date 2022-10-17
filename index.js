3135da1e60f49babe7c469d8e983cbc53ce6dc5e
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-17 13:15:49 +0600

    module scaffolding

--- index.js
+++ index.js
@@ -1,76 +1,20 @@
 // dependencies
-const http = require('http');
-const environment = require('./helpers/environments');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const { sendSMS } = require('./helpers/notifications');
-const data = require('./lib/data');
+const server = require('./lib/server');
+const worker = require('./lib/worker');
 
 // app object
 const app = {};
 
-//
-sendSMS(
-  '01766253827',
-  'Hello there! This is a top secret message! you should come to visit wwe at Brooklyn! always keep your data on! BYE',
-  (err) => {
-    console.log(`The error is ${err}`);
-  }
-);
+app.init = () => {
+  // start the server
+  server.init();
 
-// configuration
-app.config = {
-  port: 4000,
+  // start the worker
+  worker.init();
 };
 
-// testing file system
-//  crud operation check starts
-// create data
-// data.create(
-//   'test',
-//   'details',
-//   { name: 'Jiku', nationality: 'Bangladesh' },
-//   (err) => {
-//     console.log(`error was ${err}`);
-//   }
-// );
+// calling the function
+app.init();
 
-// read data
-// data.read('test', 'personalInfo', (err, data) => {
-//   console.log(err, data);
-// });
-
-// update data
-// data.update(
-//   'test',
-//   'personalInfo',
-//   {
-//     name: 'Muhammad Azizul Hoque Jiku ',
-//     nationality: 'Bangladeshi',
-//     religion: 'Islam',
-//     hometown: 'Feni',
-//   },
-//   (err) => {
-//     console.log(`error was ${err}`);
-//   }
-// );
-
-// delete data
-// data.delete('test', 'details', (err) => {
-//   console.log(err);
-// });
-
-// create server
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  // const port = app?.config?.port;
-  const port = environment.port;
-  server.listen(port, () => {
-    console.log(`server runnig at http://localhost:${port}`);
-  });
-};
-
-// handle request and response
-app.handleReqRes = handleReqRes;
-
-// starting the server
-app?.createServer();
+// exporting app
+module.exports = app;
